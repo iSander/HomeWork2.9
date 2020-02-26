d7f5e2683b77916ded314c4abab229acfbc4eae5
Alex Sander <alex.darienko@gmail.com>
2020-02-26 11:30:39 +0200

    Initial Commit

---
+++ HomeWork2.9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork2.9
+//
+//  Created by Alex Sander on 26.02.2020.
+//  Copyright © 2020 Alex Sander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
